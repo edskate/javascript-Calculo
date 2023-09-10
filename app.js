2ea0f3913ece96c25fa4b2b6a0d6e5b2a8e1ebed
edilson gomes de lima <edskateicevida@gmail.com>
2023-09-09 21:41:08 -0300

    add calculo

---
+++ app.js
@@ -0,0 +1,19 @@
+var numero1 = 10;
+var numero2 = 15;
+var resultado = numero1 + numero2;
+alert(resultado);
+
+function mostraResultado()
+{
+	var numero1 = 50;
+	var numero2 = 50;
+	var resultado = numero1 + numero2 ;
+	document.getElementById('resultado').innerHTML = resultado;
+}
+function mostraResultado1()
+{
+	var numero1 = 50;
+    var numero2 = 50;
+	var resultado1 = numero1 + numero2 ;
+	document.getElementById('resultado1').innerHTML = resultado1;
+}
